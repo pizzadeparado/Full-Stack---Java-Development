c9be914178bc2d1c20919826f0787bc823ad33cb
Cristian Cahe <cgcahe@gmail.com>
2019-11-06 12:24:54 -0300

    Optimizando

--- salvo/src/main/resources/static/web/js/games.js
+++ salvo/src/main/resources/static/web/js/games.js
@@ -1,11 +1,126 @@
 var app = new Vue({
   el: "#app",
   data: {
-    vueGames: [],
-    vueScore: [],
-    currentUser: ''
+    salvoGames: [],
+    salvoScore: [],
+    salvoUser: '',
+    registrationEmail: "",
+    registrationPassword: "",
+    loginEmail: "",
+    loginPassword: ""
   },
   methods: {
+    registration() {
+      if (
+        app.registrationEmail == "" || app.registrationPassword == ""
+      )
+        swal("Please complete the fields", {
+          closeOnClickOutside: true,
+          icon: "error",
+          buttons: false,
+          timer: 2500,
+        })
+
+      else {
+        $.post("/api/players", {
+          username: app.registrationEmail,
+          password: app.registrationPassword
+        })
+          .done(function () {
+            swal("Account created successfully. Redirecting...", {
+              closeOnClickOutside: false,
+              icon: "success",
+              buttons: false,
+              timer: 2500,
+            });
+            window.setTimeout(function () { window.location.reload() }, 2500);
+          })
+          .fail(function () {
+            swal("Sorry, we couldn't create your account. Please try again.", {
+              closeOnClickOutside: true,
+              icon: "warning",
+              buttons: false,
+              timer: 2500,
+            });
+          });
+      }
+    },
+    login() {
+      if (
+        app.loginEmail == "" || app.loginPassword == ""
+      )
+        swal("Please complete the fields", {
+          closeOnClickOutside: true,
+          icon: "error",
+          buttons: false,
+          timer: 2500,
+        })
+
+      else {
+        $.post("/api/login", {
+          username: app.loginEmail,
+          password: app.loginPassword
+        })
+          .done(function () {
+            swal("You're now logged in. Redirecting you...", {
+              closeOnClickOutside: false,
+              icon: "success",
+              buttons: false,
+              timer: 2500,
+            });
+            window.setTimeout(function () { window.location.reload() }, 2500);
+          })
+          .fail(function () {
+            swal("Incorrect username or password", {
+              closeOnClickOutside: true,
+              icon: "error",
+              buttons: false,
+              timer: 2500,
+            });
+          })
+      }
+    },
+    logout() {
+      $.post("/api/logout")
+        .done(function () {
+          swal("You're now logged out. Redirecting you...", {
+            closeOnClickOutside: false,
+            icon: "success",
+            buttons: false,
+            timer: 2500,
+          });
+          window.setTimeout(function () { window.location.reload() }, 2500);
+        })
+        .fail(function () {
+          swal("There was a problem and we couldn't log you out. Please try again.", {
+            closeOnClickOutside: true,
+            icon: "error",
+            buttons: false,
+            timer: 2500,
+          });
+          window.setTimeout(function () { window.location.reload() }, 2500);
+        });
+    },
+    createGame() {
+      $.post("/api/games")
+        .done(function (data) {
+          swal("Starting a new game in 3, 2, 1...", {
+            closeOnClickOutside: false,
+            icon: "info",
+            buttons: false,
+            timer: 2500,
+          });
+          window.setTimeout(function () { window.location.href = "game.html?gp=" + data.gamePlayerID; }, 2500);
+        })
+        .fail(function () {
+          swal("Game couldn't be created. Please try again.", {
+            closeOnClickOutside: true,
+            icon: "error",
+            buttons: false,
+            timer: 2500,
+          });
+        });
+    },
     joinGame(gameID) {
       $.post("/api/games/" + gameID + "/player")
         .done(function (data) {
@@ -30,8 +145,24 @@ var app = new Vue({
       window.setTimeout(function () {
         window.location.href = "http://localhost:8080/web/game.html?gp=" + ID;
       }, 1500);
+    },
+    newGameAlert() {
+      swal("You need to log in before starting a new game!", {
+        closeOnClickOutside: true,
+        icon: "warning",
+        buttons: false,
+        timer: 2500,
+      })
+    },
+    registrationAfterLogIn() {
+      swal("You're already logged in. You can't create a new account!", {
+        closeOnClickOutside: true,
+        icon: "info",
+        buttons: false,
+        timer: 2500,
+      })
     }
-  }
+  },
 });
 
 $(function () {
@@ -44,13 +175,12 @@ function getParameterByName(user) {
   return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
-
 /******************** GAMES LIST ********************/
 function loadGames() {
   $.get("/api/games")
     .done(function (data) {
-      app.vueGames = data.games;
-      app.currentUser = data.player;
+      app.salvoGames = data.games;
+      app.salvoUser = data.player;
     })
     .fail(function () {
       swal("Failed to load the game list. Reloading.", {
@@ -63,12 +193,11 @@ function loadGames() {
     });
 }
 
-
 /******************** SCORE LIST ********************/
 function loadScore() {
   $.get("/api/leaderboard")
     .done(function (data) {
-      app.vueScore = data;
+      app.salvoScore = data;
     })
     .fail(function () {
       swal("Failed to load the score list. Reloading.", {
@@ -79,154 +208,4 @@ function loadScore() {
       });
       window.setTimeout(function () { window.location.reload() }, 2500);
     });
-}
-
-
-/******************** CREATE GAME ********************/
-function createGame() {
-  $.post("/api/games")
-    .done(function (data) {
-      swal("Starting a new game in 3, 2, 1...", {
-        closeOnClickOutside: false,
-        icon: "info",
-        buttons: false,
-        timer: 2500,
-      });
-      window.setTimeout(function () { window.location.href = "game.html?gp=" + data.gamePlayerID; }, 2500);
-    })
-    .fail(function () {
-      swal("Game couldn't be created. Please try again.", {
-        closeOnClickOutside: true,
-        icon: "error",
-        buttons: false,
-        timer: 2500,
-      });
-    });
-}
-
-
-/******************** REGISTRATION ********************/
-function signUp() {
-  $.post("/api/players", {
-    username: document.getElementById("registrationEmail").value,
-    password: document.getElementById("registrationPassword").value
-  })
-    .done(function () {
-      swal("Account created successfully. Redirecting...", {
-        closeOnClickOutside: false,
-        icon: "success",
-        buttons: false,
-        timer: 2500,
-      });
-      window.setTimeout(function () { window.location.reload() }, 2500);
-    })
-    .fail(function () {
-      swal("Sorry, we couldn't create your account. Please try again.", {
-        closeOnClickOutside: true,
-        icon: "warning",
-        buttons: false,
-        timer: 2500,
-      });
-    });
-}
-
-
-/******************** LOG IN ********************/
-function login() {
-  if (app.currentUser == "guest") {
-    
-  }
-  $.post("/api/login", {
-    username: document.getElementById("loginEmail").value,
-    password: document.getElementById("loginPassword").value
-  })
-    .done(function () {
-      swal("You're now logged in. Redirecting you...", {
-        closeOnClickOutside: false,
-        icon: "success",
-        buttons: false,
-        timer: 2500,
-      });
-      window.setTimeout(function () { window.location.reload() }, 2500);
-    })
-    .fail(function () {
-      swal("Incorrect username or password.", {
-        closeOnClickOutside: true,
-        icon: "error",
-        buttons: false,
-        timer: 2500,
-      });
-    });
-}
-
-/******************** LOG OUT ********************/
-function logout() {
-  $.post("/api/logout")
-    .done(function () {
-      swal("You're now logged out. Redirecting you...", {
-        closeOnClickOutside: false,
-        icon: "success",
-        buttons: false,
-        timer: 2500,
-      });
-      window.setTimeout(function () { window.location.reload() }, 2500);
-    })
-    .fail(function () {
-      swal("There was a problem and we couldn't log you out. Please try again.", {
-        closeOnClickOutside: true,
-        icon: "error",
-        buttons: false,
-        timer: 2500,
-      });
-      window.setTimeout(function () { window.location.reload() }, 2500);
-    });
-}
-
-
-/******************** JOIN & RETURN ********************/
-/*
-function joinGame() {
-  $.get("/api/games")
-  window.setTimeout(function () {
-    window.location.href = "http://localhost:8080/web/game.html?gp=" + game.player[0].gamePlayerID;
-  }, 2500);
-  swal("Joining game. Please wait.", {
-    closeOnClickOutside: false,
-    icon: "info",
-    buttons: false,
-    timer: 2500,
-  });
-}
-
-function returnGame() {
-  $.get("/api/games")
-  window.setTimeout(function () {
-    window.location.href = "http://localhost:8080/web/game.html?gp=" + game.player[1].gamePlayerID;
-  }, 2500);
-  swal("Taking you back to your game. Please wait.", {
-    closeOnClickOutside: false,
-    icon: "info",
-    buttons: false,
-    timer: 2500,
-  });
-}
-*/
-
-/******************** VARIOUS ACTIONS ********************/
-function newGameAlert() {
-  swal("You need to log in before starting a new game!", {
-    closeOnClickOutside: true,
-    icon: "warning",
-    buttons: false,
-    timer: 2500,
-  })
-}
-
-function signUpAfterLogIn() {
-  swal("You're already logged in. You can't create a new account!", {
-    closeOnClickOutside: true,
-    icon: "info",
-    buttons: false,
-    timer: 2500,
-  })
 }
